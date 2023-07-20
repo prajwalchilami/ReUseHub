0830e7f46ab0c857e2008051aed1b28adfb44151
prajwalchilami <140082841+prajwalchilami@users.noreply.github.com>
2023-07-20 19:31:19 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,67 @@
+<?php include'Admin_header.php'; ?>
+<form action="">
+   <div class="row">
+    <div class="col-sm-3"></div>
+    <div class="col-sm-6">
+<div class="container">
+    <h2>Sign Up</h2>
+    <div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+        <label for=""> Full Name</label>
+            <input type="text" name="" placeholder="Enter Full Name" class="form-control">
+        </div>
+            <div class="col-sm-2"></div>
+</div>
+<br>
+<div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+        <label for="">Contact No.</label>
+            <input type="text" name="" minlength="10" maxlength="10" placeholder="Enter Contact No." class="form-control">
+        </div>
+            <div class="col-sm-2"></div>
+</div>
+<br>
+<div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+        <label for="">Email</label>
+            <input type="email" name="" placeholder="Enter  email" class="form-control">
+        </div>
+            <div class="col-sm-2"></div>
+</div>
+<br>
+<div class="row">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+        <label for="">Address</label>
+            <textarea name="" id="" class="form-control" placeholder="Enter Address" ></textarea>
+        </div>
+            <div class="col-sm-2"></div>
+</div>
+<br>
+<div class="row">
+<div class="col-sm-2"></div>
+        <div class="col-sm-4">
+        <label for="">Passsward</label>
+            <input type="passward" name="" placeholder="Enter Passward" class="form-control">
+        </div>
+
+
+
+      
+        <div class="col-sm-4">
+        <label for="">Confirm Passsward</label>
+            <input type="passward" name="" placeholder="Enter Passward" class="form-control">
+        </div>
+        <div class="col-sm-2"></div>
+           </div>
+</div>
+</div>
+<div class="col-sm-3"></div>
+</div>
+</form>
+
+
+<?php include'Admin_footer.php'; ?>
